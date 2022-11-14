f6f17b07853dbb74ef506ee5b8d241eef8d6dcf5
Harvey S <hlsamuel00@yahoo.com>
2022-11-14 09:56:57 -0500

    add new kata challenge to file codewars/11-November/11-14-2022.py

--- codewars/11-November/11-14-2022.py
+++ codewars/11-November/11-14-2022.py
@@ -56,3 +56,17 @@ def opposite(number):
         print('Please enter valid numerical value.')
         quit()
 
+#==============================================================================================================
+
+# DESCRIPTION:
+# We need a function that can transform a number (integer) into a string.
+
+# What ways of achieving this do you know?
+
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+def number_to_string(num):
+    return str(num)
