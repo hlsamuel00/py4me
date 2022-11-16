73f9bdc0151dd96db662967701611db50b72722b
Harvey S <hlsamuel00@yahoo.com>
2022-11-16 09:13:27 -0500

    update solution for file ex-09/ex-09-04.py

--- ex-09/ex-09-04.py
+++ ex-09/ex-09-04.py
@@ -1,8 +1,6 @@
 file_name = input("Enter file name: ")
 email_count = dict()
 largest = None
-count = 0
-
 
 if len(file_name) < 1:
     file_name = "mbox-short.txt"
@@ -14,10 +12,8 @@ for line in file_handle:
         email = line.split()[1]
         email_count[email] = email_count.get(email, 0) + 1
 
-emails = [*email_count.keys()]
-for email in emails:
+for email in email_count:
     if email_count[email] > email_count.get(largest, 0):
         largest = email
-        count = email_count[largest]
 
-print(largest, count)
+print(largest, email_count[largest])
