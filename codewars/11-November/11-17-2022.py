824523ac33a11f9720a816d68ac86ea13f4a306f
Harvey S <hlsamuel00@yahoo.com>
2022-11-17 15:38:38 -0500

    add new kata challenge to 11-November/11-17-2022.py

--- codewars/11-November/11-17-2022.py
+++ codewars/11-November/11-17-2022.py
@@ -42,3 +42,11 @@ def square_sum(numbers):
 
 
 #==============================================================================================================
+
+# DESCRIPTION:
+# Simple, remove the spaces from the string, then return the resultant string.
+
+def no_space(string):
+    return string.replace(' ','')
+
+#==============================================================================================================
