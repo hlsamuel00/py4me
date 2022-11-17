68514bfee12adfe641bbfca1d71ed30db1553321
Harvey S <hlsamuel00@yahoo.com>
2022-11-17 16:19:13 -0500

    add new kata challenge to 11-November/11-17-2022.py

--- codewars/11-November/11-17-2022.py
+++ codewars/11-November/11-17-2022.py
@@ -151,3 +151,26 @@ def lires(time):
     return time // 2
 
 #==============================================================================================================
+
+# DESCRIPTION:
+# Introduction
+# The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+# Task
+# Given a year, return the century it is in.
+
+# Examples
+# 1705 --> 18
+# 1900 --> 19
+# 1601 --> 17
+# 2000 --> 20
+
+import math
+def century(year):
+    return math.ceil(year / 100)
+
+# OR
+
+def century(year):
+    return -(-year//100)
+
