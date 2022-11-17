de5e6e1ba25bde54ef3e8b1b96e989e900f764cc
Harvey S <hlsamuel00@yahoo.com>
2022-11-17 12:03:22 -0500

    add new kata challenge to 11-November/11-17-2022.py

--- codewars/11-November/11-17-2022.py
+++ codewars/11-November/11-17-2022.py
@@ -18,10 +18,27 @@ def remove_char(s):
 
 
 def find_smallest_int(arr):
-    smallest = float('inf')
+    smallest = arr[0]
     for num in arr:
         if smallest > num:
             smallest = num
-    print(smallest)
+    return smallest
+
+# OR
+
+def find_smallest_int(arr):
+    return min(arr)
+
+
+#==============================================================================================================
+
+
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+def square_sum(numbers):
+    return sum( number ** 2 for number in numbers )
+
 
 #==============================================================================================================
