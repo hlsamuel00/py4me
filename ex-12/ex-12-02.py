5c774c6888c4f5134273f4ad2640a4af9a560d70
Harvey S <hlsamuel00@yahoo.com>
2022-11-18 15:35:07 -0500

    add new file ex-12/ex-12-02.py with py4e challenge

--- ex-12/ex-12-02.py
+++ ex-12/ex-12-02.py
@@ -7,9 +7,3 @@ soup = BeautifulSoup(html, 'html.parser')
 tags = soup('span')
 
 print( sum([int(tag.contents[0]) for tag in tags]) )
-
-
-# for tag in tags:
-#     sum = sum + int(tag.contents[0])
-
-# print(sum)
