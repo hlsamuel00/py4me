5f34f574b61c180baa48470a8af8b6e69ef7cb43
Harvey S <hlsamuel00@yahoo.com>
2022-11-21 01:28:06 -0500

    add new file ex-13/ex-13-03.py with py4e challenge

---
+++ ex-13/ex-13-03.py
@@ -0,0 +1,18 @@
+import urllib.request, urllib.parse
+import ssl
+import json
+
+
+service_url = 'http://py4e-data.dr-chuck.net/json?'
+address = input('Enter location: ')
+
+url = service_url + urllib.parse.urlencode({ 'address': address, 'key': 42 })
+print('Retrieving', url)
+
+data = urllib.request.urlopen(url).read().decode()
+print('Retrieved', len(data), 'characters')
+
+place_id = json.loads(data)['results'][0]['place_id']
+
+print('Place id:', place_id)
+
