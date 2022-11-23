393fe192d66be966d8ff34cc83a3473fa26ce1e4
Harvey S <hlsamuel00@yahoo.com>
2022-11-23 14:15:39 -0500

    add new file 11-November/11-23-2022.py with new kata challenge

---
+++ codewars/11-November/11-23-2022.py
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+def maps(array):
+    return [ element * 2 for element in array ]
+
+#==============================================================================================================
