099def9bba183f67488f4bcc97b110b4a4c86dd1
Harvey S <hlsamuel00@yahoo.com>
2022-11-23 14:37:22 -0500

    add new kata challenge to file 11-November/11-23-2022.py

--- codewars/11-November/11-23-2022.py
+++ codewars/11-November/11-23-2022.py
@@ -9,3 +9,25 @@ def maps(array):
     return [ element * 2 for element in array ]
 
 #==============================================================================================================
+
+# DESCRIPTION:
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+# The output should be two capital letters with a dot separating them.
+
+# It should look like this:
+
+# Sam Harris => S.H
+
+# patrick feeney => P.F
+
+def abbrev_name(name):
+    return '.'.join([part[0].upper() for part in name.split()])
+
+# OR
+
+def abbrev_name(name:str):
+    first,last = name.upper().split()
+    return f'{first[0]}.{last[0]}'
+
+#=============================================================================================================
