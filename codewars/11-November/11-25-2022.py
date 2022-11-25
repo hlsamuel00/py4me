88c798504c39eab59de18b8bfc58ae9bfee403f2
Harvey S <hlsamuel00@yahoo.com>
2022-11-25 15:38:25 -0500

    add new kata challenge to file 11-November/11-25-2022.py

--- codewars/11-November/11-25-2022.py
+++ codewars/11-November/11-25-2022.py
@@ -25,3 +25,27 @@ def count_positives_sum_negatives(nums):
         if num > 0:
             count += 1
     return [count, sum]
+
+#==============================================================================================================
+
+# DESCRIPTION:
+# Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+# Example:
+# n= 5, m=5: 25
+# n=-5, m=5:  0
+# Waiting for translations and Feedback! Thanks!
+
+def paperwork(classmates, pages):
+    if classmates < 1 or pages < 1:
+        return 0
+    return classmates * pages
+
+# OR
+
+def paperwork(classmates, pages):
+    return max(classmates, 0) * max(pages, 0)
+
+#==============================================================================================================
