01eb02c7dc9b3c43d2f006dee31435e97819f3f1
Harvey S <hlsamuel00@yahoo.com>
2022-11-27 19:57:41 -0800

    add new kata challenge to file 11-November/11-27-2022.py

---
+++ codewars/11-November/11-27-2022.py
@@ -0,0 +1,11 @@
+# DESCRIPTION:
+# Write a function which calculates the average of the numbers in a given list.
+
+# Note: Empty arrays should return 0.
+
+def find_average(numbers):
+    return sum(numbers)/len(numbers)
+
+# OR
+
+from statistics import mean as find_average
