e992d60560e98ff5f05819c33e87d2571f9643a9
Harvey S <hlsamuel00@yahoo.com>
2022-12-12 10:16:17 -0500

    add new kata challenge to file 12-December/12-12-2022.py

--- codewars/12-December/12-12-2022.py
+++ codewars/12-December/12-12-2022.py
@@ -12,3 +12,29 @@ def greet(name, owner):
 
 
 #==============================================================================================================
+
+# DESCRIPTION:
+# Rock Paper Scissors
+# Let's play! You have to return which player won! In case of a draw return Draw!.
+
+# Examples(Input1, Input2 --> Output):
+
+# "scissors", "paper" --> "Player 1 won!"
+# "scissors", "rock" --> "Player 2 won!"
+# "paper", "paper" --> "Draw!"
+
+def rps(p1, p2):
+    choices = ['rock', 'scissors', 'paper']
+    if (choices.index(p1) == (choices.index(p2) - 1) % 3):
+        return 'Player 1 won!'
+    elif(choices.index(p2) == (choices.index(p1) - 1) % 3):
+        return 'Player 2 won!'
+    else:
+        return 'Draw!'
+
+# OR
+
+def rps(p1, p2):
+    hand = {'rock':0, 'paper':1, 'scissors':2}
+    results = ['Draw!', 'Player 1 won!', 'Player 2 won!']
+    return results[hand[p1] - hand[p2]]
