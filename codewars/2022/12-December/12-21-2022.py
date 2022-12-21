8beab7d3697a7dee7fb3e610b046b2c0caf1801b
Harvey S <hlsamuel00@yahoo.com>
2022-12-21 13:11:48 -0500

    add new file 2022/12-December/12-21-2022.py with kata challenge

---
+++ codewars/2022/12-December/12-21-2022.py
@@ -0,0 +1,18 @@
+# DESCRIPTION:
+# Task
+# Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+# The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+# Mind the input validation.
+
+# Example
+# { 6, 2, 1, 8, 10 } => 16
+# { 1, 1, 11, 2, 3 } => 6
+
+def sum_array(arr):
+    if arr == None or len(arr) < 3:
+        return 0
+    else: 
+        arr.sort()
+        return sum(arr[1:-1])
