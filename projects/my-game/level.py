6b0665c573cd0ec224ba2f89edaac14d823da677
Harvey S <hlsamuel00@yahoo.com>
2023-01-05 23:48:04 -0500

    update level and player for player movement and sprite updates

--- projects/my-game/level.py
+++ projects/my-game/level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -21,10 +22,12 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if column == 'x': Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
-                if column == 'p': Player((x,y), [self.visible_sprites])
+                if column == 'p': self.player = Player((x,y), [self.visible_sprites])
 
 
 
     def run(self):
         # update and draw game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
