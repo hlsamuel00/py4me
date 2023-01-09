c5e74167a58ae716a295d6c8e502954cab20f4d2
Harvey S <hlsamuel00@yahoo.com>
2023-01-09 14:51:06 -0500

    update level file for player map and gitignore to ignore large image files

--- projects/my-game/code/level.py
+++ projects/my-game/code/level.py
@@ -17,12 +17,14 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for (row_index, row) in enumerate(WORLD_MAP):
-            for(col_index, column) in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if column == 'x': Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
-                if column == 'p': self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
+        # for (row_index, row) in enumerate(WORLD_MAP):
+        #     for(col_index, column) in enumerate(row):
+        #         x = col_index * TILESIZE
+        #         y = row_index * TILESIZE
+        #         if column == 'x': Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
+
+        
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
 
 
@@ -39,12 +41,20 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
-        self.offset = pygame.math.Vector2(100, 100)
+        self.offset = pygame.math.Vector2()
+
+        # creating the floor
+        self.floor_surface = pygame.image.load('./graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0, 0))
 
     def custom_draw(self, player): # helps create camera placement for player
         # get player offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_position = self.floor_rect.topleft- self.offset
+        self.display_surface.blit(self.floor_surface, floor_offset_position)
 
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_position = sprite.rect.topleft - self.offset
