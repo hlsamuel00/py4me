c30081f79caf795ff21e6dbddfb967fffe213512
Harvey S <hlsamuel00@yahoo.com>
2023-01-10 13:39:23 -0500

    add new leetcode solution to file 2023/01-January/01-10-2023.py

--- codewars/2023/01-January/01-10-2023.py
+++ codewars/2023/01-January/01-10-2023.py
@@ -63,3 +63,55 @@ class Solution:
             if isBadVersion(mid): n_end = mid
             else: n_start = mid + 1
         return n_start
+
+
+#==============================================================================================================
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums contains distinct values sorted in ascending order.
+# -104 <= target <= 104
+
+class Solution:
+    def searchInsert(self, nums: list[int], target: int, idx = None):
+        if target not in nums:
+            for num in nums:
+                if num > target: 
+                    idx = nums.index(num) 
+                    break
+            return idx if type(idx) is int else len(nums)
+        else:
+            return nums.index(target)
+
+
+# OR!!
+
+class Solution:
+    def searchInsert(self, nums: list[int], target:int):
+        if target in nums:
+            return nums.index(target)
+        else: 
+            nums.append(target)
+            return sorted(nums).index(target)
