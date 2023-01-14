08ef4215c7bfe7838e25001a20593e92812ade1d
Harvey S <hlsamuel00@yahoo.com>
2023-01-14 15:12:49 -0500

    add new file codewars/2023/01-January/01-14-2023.py with leetcode challenge solution

---
+++ codewars/2023/01-January/01-14-2023.py
@@ -0,0 +1,31 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+ 
+# Example 1:
+#     Input: head = [1,2,3,4,5]
+#     Output: [3,4,5]
+#     Explanation: The middle node of the list is node 3.
+
+# Example 2:
+#     Input: head = [1,2,3,4,5,6]
+#     Output: [4,5,6]
+#     Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+# Constraints:
+#     The number of nodes in the list is in the range [1, 100].
+#     1 <= Node.val <= 100
+
+from math import ceil
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        my_list = []
+        while head is not None:
+            my_list.append(head)
+            head = head.next
+        
+        return my_list[ceil((len(my_list) - 1) / 2)]
+
+#==============================================================================================================
+
