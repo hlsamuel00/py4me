46bff6d6933b2b31ffe560fdf4f6a0619885ec9e
Harvey S <hlsamuel00@yahoo.com>
2023-01-14 16:02:32 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-14-2023.py

--- codewars/2023/01-January/01-14-2023.py
+++ codewars/2023/01-January/01-14-2023.py
@@ -21,11 +21,61 @@ from math import ceil
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         my_list = []
-        while head is not None:
+        while head:
             my_list.append(head)
             head = head.next
         
         return my_list[ceil((len(my_list) - 1) / 2)]
 
+
+# OR
+
+class Solution:
+    def middleNode(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
 #==============================================================================================================
 
+
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+# Example 1:
+#     Input: head = [1,2,3,4,5], n = 2
+#     Output: [1,2,3,5]
+
+# Example 2:
+#     Input: head = [1], n = 1
+#     Output: []
+
+# Example 3:
+#     Input: head = [1,2], n = 1
+#     Output: [1]
+ 
+# Constraints:
+#     The number of nodes in the list is sz.
+#     1 <= sz <= 30
+#     0 <= Node.val <= 100
+#     1 <= n <= sz
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        for _ in range(n):
+            fast = fast.next
+        
+        if not fast:
+            return head.next
+        
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+        
+        return head
