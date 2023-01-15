3c80fc086b722dee79e5f15f62a6959270b19149
Harvey S <hlsamuel00@yahoo.com>
2023-01-15 14:13:02 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-15-2023.py

--- codewars/2023/01-January/01-15-2023.py
+++ codewars/2023/01-January/01-15-2023.py
@@ -62,10 +62,8 @@ class Solution:
         window = len(s1) # setting the window to the length of the test string
         s1_counter = Counter(s1) # create a count of all letters in the test string
         
-        print(len(s2) - window + 1, len(s2))
         for i in range(len(s2) - window + 1): # creating boundaries so check remains within length of string
-            s2_counter = Counter(s2[i : i + window]) # creating a new count of all of the letters in the string
-            if s2_counter == s1_counter: # conditional testing count of letters in string with count in test string
+            if s1_counter == Counter(s2[i : i + window]): # creating a new count of all of the letters in the string and testing count of letters in string with the letter count in test string
                 return True # return true if they match
             
         return False # if loop wasn't broken, no permutation exists
