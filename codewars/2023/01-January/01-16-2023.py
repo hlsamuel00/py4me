7c376ba9a6305f2438868e4829a33d5b7cae0d3b
Harvey S <hlsamuel00@yahoo.com>
2023-01-16 16:00:14 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-16-2023.py

--- codewars/2023/01-January/01-16-2023.py
+++ codewars/2023/01-January/01-16-2023.py
@@ -50,3 +50,62 @@ class Solution:
         return image
 
 #==============================================================================================================
+
+# You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+# The area of an island is the number of cells with a value 1 in the island.
+
+# Return the maximum area of an island in grid. If there is no island, return 0.
+
+# Example 1:
+#     Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+#     Output: 6
+#     Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+# Example 2:
+#     Input: grid = [[0,0,0,0,0,0,0,0]]
+#     Output: 0
+ 
+
+# Constraints:
+#     m == grid.length
+#     n == grid[i].length
+#     1 <= m, n <= 50
+#     grid[i][j] is either 0 or 1.
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
+        maxArea = 0
+
+        # verifiy if location is land
+        def is_land(row, column):
+            # ensure that location is within bounds of the grid
+            if row < 0 or column < 0 or row >= len(grid) or column >= len(grid[0]): return False
+
+            # ensure that location is indeed land
+            if not grid[row][column]: return False
+
+            # if we've made it this far, location is indeed land
+            return True
+
+        # calculate area of land found
+        def calc_area(row, column):
+            # determine if location is land
+            if not is_land(row,column): return 0
+
+            grid[row][column] = 0
+            #determine surrounding if areas are land
+            up, down, left, right = calc_area(row - 1, column), calc_area(row + 1, column), calc_area(row, column - 1), calc_area(row, column + 1)
+
+            # return total count of area
+            return up + down + left + right + 1
+
+
+        # run for loop through the grid for each point
+        for r_idx in range(len(grid)):
+            for c_idx in range(len(grid[r_idx])):
+                area = calc_area(r_idx,c_idx)
+                if area > maxArea: maxArea = area
+        
+        return maxArea
