3f5ae2a0ca88cd7e4438f0240401bd24a3852365
Harvey S <hlsamuel00@yahoo.com>
2023-01-18 11:49:51 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-18-2023.py

--- codewars/2023/01-January/01-18-2023.py
+++ codewars/2023/01-January/01-18-2023.py
@@ -112,3 +112,33 @@ class Solution:
 
 #==============================================================================================================
 
+# Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+# Example 1:
+#     Input: n = 234
+#     Output: 15 
+#     Explanation: 
+#     Product of digits = 2 * 3 * 4 = 24 
+#     Sum of digits = 2 + 3 + 4 = 9 
+#     Result = 24 - 9 = 15
+
+# Example 2:
+#     Input: n = 4421
+#     Output: 21
+#     Explanation: 
+#     Product of digits = 4 * 4 * 2 * 1 = 32 
+#     Sum of digits = 4 + 4 + 2 + 1 = 11 
+#     Result = 32 - 11 = 21
+ 
+# Constraints:
+# 1 <= n <= 10^5
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        digit_sum, digit_product = 0, 1
+
+        for digit in list(str(n)):
+            digit_sum += int(digit)
+            digit_product *= int(digit)
+        
+        return digit_product - digit_sum
