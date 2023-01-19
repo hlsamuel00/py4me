902be65cbf68e2219ff81ab05b992e88ad13e098
Harvey S <hlsamuel00@yahoo.com>
2023-01-19 16:43:26 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-19-2023.py

--- codewars/2023/01-January/01-19-2023.py
+++ codewars/2023/01-January/01-19-2023.py
@@ -69,3 +69,34 @@ class Solution:
         head.next = None
         
         return newNode
+
+#==============================================================================================================
+
+# Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+# Example 1:
+#     Input: nums = [2,1,2]
+#     Output: 5
+#     Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+
+# Example 2:
+#     Input: nums = [1,2,1,10]
+#     Output: 0
+#     Explanation: 
+#     You cannot use the side lengths 1, 1, and 2 to form a triangle.
+#     You cannot use the side lengths 1, 1, and 10 to form a triangle.
+#     You cannot use the side lengths 1, 2, and 10 to form a triangle.
+#     As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+ 
+
+# Constraints:
+#     3 <= nums.length <= 104
+#     1 <= nums[i] <= 106
+
+class Solution:
+    def largestPerimeter(self, nums: list[int]) -> int:
+        nums.sort(reverse = True)
+        for i in range(3, len(nums) + 1):
+            if nums[i-3] < nums[i - 2] + nums[i - 1]:
+                return sum(nums[i - 3: i])
+        return 0
