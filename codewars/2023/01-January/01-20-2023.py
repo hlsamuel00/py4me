f8acb36ac21ae2d948e83c4b96dce31c82e36a64
Harvey S <hlsamuel00@yahoo.com>
2023-01-20 10:07:59 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-20-2023.py

--- codewars/2023/01-January/01-20-2023.py
+++ codewars/2023/01-January/01-20-2023.py
@@ -51,3 +51,38 @@ class Solution:
     def permute(self, nums: list[int]) -> list[list[int]]:
         return list(permutations(nums))
 
+#==============================================================================================================
+
+# Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+
+# Return a list of all possible strings we could create. Return the output in any order.
+
+# Example 1:
+#     Input: s = "a1b2"
+#     Output: ["a1b2","a1B2","A1b2","A1B2"]
+
+# Example 2:
+#     Input: s = "3z4"
+#     Output: ["3z4","3Z4"]
+ 
+
+# Constraints:
+#     1 <= s.length <= 12
+#     s consists of lowercase English letters, uppercase English letters, and digits.
+
+class Solution:
+    def letterCasePermutation(self, s:str) -> list[str]:
+        
+        def casePermute(string:str, idx = 0):
+            if len(string) == len(s):
+                ans.append(string)
+            else:
+                if s[idx].isalpha():
+                    casePermute(string + s[idx].swapcase(), idx + 1)
+                casePermute(string + s[idx], idx + 1)
+            
+        ans = []
+        casePermute()
+        return ans
+
+#==============================================================================================================
