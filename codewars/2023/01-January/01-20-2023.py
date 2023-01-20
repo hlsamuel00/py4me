cb5bb0d90aabbd7149dce4457414dc6ad9d4a2ab
Harvey S <hlsamuel00@yahoo.com>
2023-01-20 10:26:36 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-20-2023.py

--- codewars/2023/01-January/01-20-2023.py
+++ codewars/2023/01-January/01-20-2023.py
@@ -86,3 +86,52 @@ class Solution:
         return ans
 
 #==============================================================================================================
+
+# There is a function signFunc(x) that returns:
+
+# 1 if x is positive.
+# -1 if x is negative.
+# 0 if x is equal to 0.
+
+# You are given an integer array nums. Let product be the product of all values in the array nums.
+
+# Return signFunc(product). 
+
+# Example 1:
+#     Input: nums = [-1,-2,-3,-4,3,2,1]
+#     Output: 1
+#     Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+
+# Example 2:
+#     Input: nums = [1,5,0,2,-3]
+#     Output: 0
+#     Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+
+# Example 3:
+#     Input: nums = [-1,1,-1,1,-1]
+#     Output: -1
+#     Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+
+class Solution:
+    def arraySign(self, nums: list[int]) -> int:
+        product = 1
+        for num in nums:
+            product *= num
+        
+        signFunc = lambda x: (x > 0) - (x < 0)
+    
+        return signFunc(product)
+
+# OR
+
+class Solution:
+    def arraySign(self, nums: list[int]) -> int:
+        ans = 1
+        for num in nums:
+            if not num: return 0
+            if num < 0: ans *= -1
+        
+        return ans
+
+#==============================================================================================================
+
