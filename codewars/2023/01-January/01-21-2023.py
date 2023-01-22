9b8da63c34569c4ebb1e24009350962a82122bab
Harvey S <hlsamuel00@yahoo.com>
2023-01-21 20:35:58 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-21-2023.py

--- codewars/2023/01-January/01-21-2023.py
+++ codewars/2023/01-January/01-21-2023.py
@@ -186,3 +186,28 @@ class Solution:
         return [ans.get(e, -1) for e in nums1]
 
 #==============================================================================================================
+
+# You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+# Example 1:
+#     Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+#     Output: true
+
+# Example 2:
+#     Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+#     Output: false
+
+# Constraints:
+#     2 <= coordinates.length <= 1000
+#     coordinates[i].length == 2
+#     -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+#     coordinates contains no duplicate point.
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        (x1, y1), (x2, y2) = coordinates[0:2]
+        
+        for x3, y3 in coordinates[2:]:
+            if (y2 - y1) * (x3 - x1) != (y3 - y1) * (x2 - x1): return False
+        
+        return True
