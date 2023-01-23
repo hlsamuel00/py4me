46a54b903536d7c429ac1be30ad3f46b90ac224f
Harvey S <hlsamuel00@yahoo.com>
2023-01-23 12:21:37 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-23-2023.py

--- codewars/2023/01-January/01-23-2023.py
+++ codewars/2023/01-January/01-23-2023.py
@@ -100,15 +100,50 @@ class Solution:
 
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        total, back, middle = 0, -1, len(mat) // 2
+        total = 0
         
-        for front in range(len(mat)):
-            total += mat[front][front] + mat[front][back]
-            back -= 1
+        for idx in range(len(mat)):
+            total += mat[idx][idx] + mat[idx][-(idx + 1)]
         
-        if len(mat) % 2: total -= mat[middle][middle]
+        if len(mat) % 2: 
+            middle = len(mat) // 2
+            total -= mat[middle][middle]
         
         return total
 
 #==============================================================================================================
 
+# In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+
+# You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+
+# The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+# If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+# Example 1:
+#     Input: mat = [[1,2],[3,4]], r = 1, c = 4
+#     Output: [[1,2,3,4]]
+
+# Example 2:
+#     Input: mat = [[1,2],[3,4]], r = 2, c = 4
+#     Output: [[1,2],[3,4]]
+ 
+# Constraints:
+#     m == mat.length
+#     n == mat[i].length
+#     1 <= m, n <= 100
+#     -1000 <= mat[i][j] <= 1000
+#     1 <= r, c <= 300
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if len(mat[0]) * len(mat) != r * c: return mat
+
+        mat = [item for sublist in mat for item in sublist]
+    
+        temp_mat = []
+        for idx in range(r):
+            temp_mat.append(mat[idx * c: idx * c + c])
+        
+        return temp_mat
