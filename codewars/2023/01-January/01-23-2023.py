8fe8aa5698922dc1f249df3ca48cc51155369c54
Harvey S <hlsamuel00@yahoo.com>
2023-01-23 10:18:11 -0500

    add new leetcode challenge solution to file codewars/2023/01-January/01-23-2023.py

--- codewars/2023/01-January/01-23-2023.py
+++ codewars/2023/01-January/01-23-2023.py
@@ -27,3 +27,45 @@ class Solution:
 
 #==============================================================================================================
 
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+# Example 1:
+#     Input: nums = [2,2,1]
+#     Output: 1
+
+# Example 2:
+#     Input: nums = [4,1,2,1,2]
+#     Output: 4
+
+# Example 3:
+#     Input: nums = [1]
+#     Output: 1
+ 
+# Constraints:
+#     1 <= nums.length <= 3 * 104
+#     -3 * 104 <= nums[i] <= 3 * 104
+#     Each element in the array appears twice except for one element which appears only once.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        back = -1
+        for idx in range(len(nums)):
+            if nums.count(nums[idx]) == 1: return nums[idx]
+            if nums.count(nums[back]) == 1: return nums[back]
+            back -= 1
+
+# OR
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        answer = 0
+        
+        for num in nums:
+            answer ^= num
+        
+        return answer
+
+#==============================================================================================================
+
