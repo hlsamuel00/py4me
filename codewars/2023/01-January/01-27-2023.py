4147ed190ce2e4bd984ba3606ac44fc3ae14ec6c
Harvey S <hlsamuel00@yahoo.com>
2023-01-27 11:26:06 -0500

    update leetcode solution on file codewars/2023/01-January/01-27-2023.py

--- codewars/2023/01-January/01-27-2023.py
+++ codewars/2023/01-January/01-27-2023.py
@@ -119,4 +119,16 @@ class Solution:
         
         return s_count == t_count
 
+# OR
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            if s.count(letter) != t.count(letter): return False
+        
+        return True
+
 #==============================================================================================================
+
