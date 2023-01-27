6568d813e1087e8133e6c264370ffb0f90a97889
Harvey S <hlsamuel00@yahoo.com>
2023-01-27 10:52:15 -0500

    update file codewars/2023/01-January/01-27-2023.py with new leetcode challenge solution

--- codewars/2023/01-January/01-27-2023.py
+++ codewars/2023/01-January/01-27-2023.py
@@ -81,3 +81,42 @@ class MyQueue:
         
     def empty(self) -> bool:
         return not self.in_stack and not self.out_stack
+
+#==============================================================================================================
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+# Example 1:
+#     Input: s = "anagram", t = "nagaram"
+#     Output: true
+
+# Example 2:
+#     Input: s = "rat", t = "car"
+#     Output: false
+ 
+# Constraints:
+#     1 <= s.length, t.length <= 5 * 104
+#     s and t consist of lowercase English letters.
+
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+# OR
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        s_count, t_count = {}, {}
+        for idx in range(len(s)):
+            s_count[s[idx]] = s_count.get(s[idx], 0) + 1
+            t_count[t[idx]] = t_count.get(t[idx], 0) + 1
+
+        
+        return s_count == t_count
+
+#==============================================================================================================
