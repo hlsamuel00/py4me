9934ebc0cef50d33fc8f3bcd26a8b4e908ba05e0
Harvey S <hlsamuel00@yahoo.com>
2023-02-04 16:24:22 -0500

    update file codewars/2023/02-Febrary/02-04-2023.py with new kata challenge

--- codewars/2023/02-February/02-04-2023.py
+++ codewars/2023/02-February/02-04-2023.py
@@ -8,3 +8,25 @@
 
 def filter_list(l):
     return [ val for val in l if type(val) is int ]
+
+#============================================================================================================
+
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+# Example: (Input --> Output)
+
+# "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+# isIsogram "Dermatoglyphics" = true
+# isIsogram "moose" = false
+# isIsogram "aba" = false
+
+def is_isogram(string):
+    for letter in string.lower():
+        if string.lower().count(letter) > 1:
+            return False        
+    
+    return True
+
+#==============================================================================================================
+
