8e33267d05879b1e5477a315940d6b9f1aca442e
Harvey S <hlsamuel00@yahoo.com>
2023-02-07 09:51:15 -0500

    update file codewars/2023/02-Febrary/02-07-2023.py with new kata challenge solution

--- codewars/2023/02-February/02-07-2023.py
+++ codewars/2023/02-February/02-07-2023.py
@@ -4,3 +4,17 @@
 
 def find_short(string: str) -> str:
     return min(len(word) for word in string.split(' '))
+
+#==============================================================================================================
+
+# DESCRIPTION:
+# This time no story, no theory. The examples below show you how to write function accum:
+
+# Examples:
+#     accum("abcd") -> "A-Bb-Ccc-Dddd"
+#     accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+#     accum("cwAt") -> "C-Ww-Aaa-Tttt"
+#     The parameter of accum is a string which includes only letters from a..z and A..Z
+
+def accum(string: str) -> str:
+    return '-'.join((letter * idx).capitalize() for idx, letter in enumerate(string, 1))
