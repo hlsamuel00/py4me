b442e486b4ae12c42fd7bdd3f9427661dfb0c2c2
Harvey S <hlsamuel00@yahoo.com>
2023-02-16 12:50:27 -0500

    update file codewars/2023/02-February/02-16-2023.py with new kata challenge solution

--- codewars/2023/02-February/02-16-2023.py
+++ codewars/2023/02-February/02-16-2023.py
@@ -40,3 +40,35 @@ def nb_year(p0, percent, aug, p, years = 0):
         return nb_year((p0 + int(p0 * (percent / 100)) + aug), percent, aug, p, years + 1)
     else:
         return years
+    
+#==============================================================================================================
+
+# Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+# Examples:
+#     a = "xyaabbbccccdefww"
+#     b = "xxxxyyyyabklmopq"
+#     longest(a, b) -> "abcdefklmopqwxy"
+
+#     a = "abcdefghijklmnopqrstuvwxyz"
+#     longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+def longest(a1, a2):
+    return ''.join(sorted(set(a1).union(set(a2))))
+
+# OR
+
+def longest(string1: str, string2: str) -> str:
+    return ''.join(sorted(set(string1 + string2)))
+
+# OR
+
+def longest(str1: str, str2: str) -> str:
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    sorted_str = ''
+
+    for letter in alphabet:
+        if letter in (str1 + str2).lower():
+            sorted_str += letter
+    
+    return sorted_str
