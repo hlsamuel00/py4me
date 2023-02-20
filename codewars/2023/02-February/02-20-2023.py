ef30756869ff555535d19019c062e0a541b0956c
Harvey S <hlsamuel00@yahoo.com>
2023-02-20 14:21:54 -0500

    update new file codewars/2023/02-February/02-20-2023.py with new solution

--- codewars/2023/02-February/02-20-2023.py
+++ codewars/2023/02-February/02-20-2023.py
@@ -34,3 +34,22 @@ def dig_pow(num: int, power: int) -> int:
 def dig_pow(num: int, power: int) -> int:
     total = sum(int(str(num)[idx]) ** (idx + power) for idx in range(len(str(num)))) / num
     return total if total.is_integer() else -1
+
+
+#==============================================================================================================
+
+# Task
+# You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+# Examples
+# [7, 1]  =>  [1, 7]
+# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+def sort_array(source_array: list[int]) -> list[int]:
+    odds = sorted([num for num in source_array if num % 2])
+    return [odds.pop(0) if num % 2 else num for num in source_array]
+
+#==============================================================================================================
+
+
