8e04fc54567459b6599a84723967b7de5d82cccf
Harvey S <hlsamuel00@yahoo.com>
2023-02-20 15:26:50 -0500

    update new file codewars/2023/02-February/02-20-2023.py with new kata challenge

--- codewars/2023/02-February/02-20-2023.py
+++ codewars/2023/02-February/02-20-2023.py
@@ -53,3 +53,20 @@ def sort_array(source_array: list[int]) -> list[int]:
 #==============================================================================================================
 
 
+# DESCRIPTION:
+# You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+# The returned value must be a string, and have "***" between each of its letters.
+
+# You should not remove or add elements from/to the array.
+
+def two_sort(array):
+    return '***'.join(list(sorted(array)[0]))
+
+# OR
+
+def two_sort(array: list[str]) -> str:
+    return '***'.join(min(array))
+
+#=============================================================================================================
+
