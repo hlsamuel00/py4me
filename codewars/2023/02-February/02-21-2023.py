56ffd0ece620b29b2101e4e7b8709f9aff2a646e
Harvey S <hlsamuel00@yahoo.com>
2023-02-21 15:33:56 -0500

    update new file codewars/2023/02-February/02-21-2023.py with new kata challenge

--- codewars/2023/02-February/02-21-2023.py
+++ codewars/2023/02-February/02-21-2023.py
@@ -10,3 +10,23 @@ def flatten_and_sort(array: list[list[int]]) -> list[int]:
 
 #==============================================================================================================
 
+# DESCRIPTION:
+# Complete the method which accepts an array of integers, and returns one of the following:
+
+    # "yes, ascending" - if the numbers in the array are sorted in an ascending order
+    # "yes, descending" - if the numbers in the array are sorted in a descending order
+    # "no" - otherwise
+# You can assume the array will always be valid, and there will always be one correct answer.
+
+def is_sorted_and_how(arr):
+    ascending, descending = 0,0
+    
+    for idx in range(1, len(arr)):
+        if arr[idx - 1] > arr[idx]:
+            descending += 1
+        if arr[idx - 1] < arr[idx]:
+            ascending += 1
+
+    return 'no' if descending and ascending else 'yes, ascending' if ascending else 'yes, descending'
+
+#==============================================================================================================
