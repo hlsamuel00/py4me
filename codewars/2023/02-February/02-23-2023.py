2996b91baca5ff4761f132796ff3d085f04980a5
Harvey S <hlsamuel00@yahoo.com>
2023-02-23 12:49:14 -0500

    add new file codewars/2023/02-February/02-23-2023.py with new solution

--- codewars/2023/02-February/02-23-2023.py
+++ codewars/2023/02-February/02-23-2023.py
@@ -6,3 +6,21 @@ def sortme(names):
 
 #==============================================================================================================
 
+# Task
+# You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+
+# Note: the scores will always be unique (so no duplicate values)
+
+# Examples
+# {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+# {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+# {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+
+def my_languages(results: dict) -> list:
+    filtered_results = [key for key,value in results.items() if value > 59]
+    return sorted(filtered_results, key = results.get, reverse = True)
+
+# OR
+
+def my_languages(results: dict) -> list:
+    return sorted([key for key,value in results.items() if value > 59], key = results.get, reverse = True)
