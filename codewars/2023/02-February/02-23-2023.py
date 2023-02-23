69d18e8a1d36fbb47ae10c8ea7c676557526baf6
Harvey S <hlsamuel00@yahoo.com>
2023-02-23 09:17:00 -0500

    add new file codewars/2023/02-February/02-23-2023.py with new solution

---
+++ codewars/2023/02-February/02-23-2023.py
@@ -0,0 +1,8 @@
+# DESCRIPTION:
+# Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+def sortme(names):
+    return sorted(names)
+
+#==============================================================================================================
+
