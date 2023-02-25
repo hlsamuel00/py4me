95adde8d9027771b03f1c17056b658b3b64987c9
Harvey S <hlsamuel00@yahoo.com>
2023-02-24 22:24:36 -0500

    update new file codewars/2023/02-February/02-24-2023.py with new solution

--- codewars/2023/02-February/02-24-2023.py
+++ codewars/2023/02-February/02-24-2023.py
@@ -30,4 +30,60 @@ def sort_my_string(s:str) -> str:
     return f'{evens} {odds}'
 
 #==============================================================================================================
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+# An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+# Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+ 
+# Example 1:
+#     Input: nums = [1,2,2,3]
+#     Output: true
+
+# Example 2:
+#     Input: nums = [6,5,4,4]
+#     Output: true
+
+# Example 3:
+#     Input: nums = [1,3,2]
+#     Output: false
+ 
+# Constraints:
+#     1 <= nums.length <= 105
+#     -105 <= nums[i] <= 105
+
+
+class Solution:
+    def isMonotonic(self, nums: list[int]) -> bool:
+        ascending, descending = 0,0
+        for idx in range(1, len(nums)):
+            if nums[idx - 1] < nums[idx]:
+                ascending += 1
+            
+            if nums[idx - 1] > nums[idx]:
+                descending += 1
+
+        return ascending and not descending or descending and not ascending or not ascending and not descending
+    
+    # OR
+
+class Solution:
+    def isMonotonic(self, nums: list[int]) -> bool:
+        monotonic = True
+
+        if nums[-1] <= nums[0]:
+            for idx in range(1, len(nums)):
+                if nums[idx - 1] < nums[idx]:
+                    monotonic = False
+                    break
+
+        if nums[-1] >= nums[0]:
+            for idx in range(1, len(nums)):
+                if nums[idx - 1] > nums[idx]:
+                    monotonic = False
+                    break
+        
+        return monotonic
+
+#==============================================================================================================
 
