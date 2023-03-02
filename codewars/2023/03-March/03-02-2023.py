b66a0dd2eeff9d9d4e2eb0005330498910ca3c03
Harvey S <hlsamuel00@yahoo.com>
2023-03-02 16:22:43 -0500

    update file codewars/2023/03-March/03-02-2023.py with new leetcode solution

--- codewars/2023/03-March/03-02-2023.py
+++ codewars/2023/03-March/03-02-2023.py
@@ -29,3 +29,45 @@ class Solution:
 
 #==============================================================================================================
 
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+# A word is a maximal substring consisting of non-space characters only.
+
+# Example 1:
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+
+# Example 2:
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+
+# Example 3:
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+
+# Constraints:
+# 1 <= s.length <= 104
+# s consists of only English letters and spaces ' '.
+# There will be at least one word in s.
+
+from re import findall
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(findall('\w+', s)[-1])
+    
+# OR
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        for idx in range(-1, -(len(s) + 1), -1):
+            if s[idx] == ' ':
+                if count:
+                    return count
+                continue
+            count += 1
+
+        return count
