170c3313a1eb6caceb92f281b42e011178b19bfa
Harvey S <hlsamuel00@yahoo.com>
2023-03-03 10:57:34 -0500

    update file codewars/2023/03-March/03-03-2023.py with new leetcode solution

--- codewars/2023/03-March/03-03-2023.py
+++ codewars/2023/03-March/03-03-2023.py
@@ -24,9 +24,48 @@ class Solution:
 
             for c_idx in range(len(matrix[0])):
                 new_row.append(dummy_matrix[(len(matrix[0]) - 1) - c_idx][r_idx])
-            print(dummy_matrix)
             
             matrix[r_idx] = new_row
 
 #==============================================================================================================
 
+# Given two n x n binary matrices mat and target, return true if it is possible to make mat equal to target by rotating mat in 90-degree increments, or false otherwise.
+
+# Example 1:
+#     Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
+#     Output: true
+#     Explanation: We can rotate mat 90 degrees clockwise to make mat equal target.
+
+# Example 2:
+#     Input: mat = [[0,1],[1,1]], target = [[1,0],[0,1]]
+#     Output: false
+#     Explanation: It is impossible to make mat equal to target by rotating mat.
+
+# Example 3:
+#     Input: mat = [[0,0,0],[0,1,0],[1,1,1]], target = [[1,1,1],[0,1,0],[0,0,0]]
+#     Output: true
+#     Explanation: We can rotate mat 90 degrees clockwise two times to make mat equal target.
+
+# Constraints:
+#     n == mat.length == target.length
+#     n == mat[i].length == target[i].length
+#     1 <= n <= 10
+#     mat[i][j] and target[i][j] are either 0 or 1.
+
+class Solution:
+    def findRotation(self, mat: list[list[int]], target: list[list[int]]) -> bool:
+        for _ in range(4):
+            self.rotate(mat)
+            if mat == target: return True
+        
+        return False
+    
+    def rotate(self, mat: list[list[int]]) -> None:
+        temp_mat = mat[:]
+
+        for r_idx in range(len(mat)):
+            new_row = []
+            for c_idx in range(len(mat[0])):
+                new_row.append(temp_mat[(len(mat[0]) - 1) - c_idx][r_idx])
+
+            mat[r_idx] = new_row
