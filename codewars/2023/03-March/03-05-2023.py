92d06ba531b048b1df3e31504963cdd89a94313a
Harvey S <hlsamuel00@yahoo.com>
2023-03-05 09:04:50 -0500

    add new file codewars/2023/03-March/03-05-2023.py with new kata solution

---
+++ codewars/2023/03-March/03-05-2023.py
@@ -0,0 +1,9 @@
+# DESCRIPTION:
+# Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+# If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+# All inputs will be valid.
+
+def last(s: str) -> list[str]:
+    return sorted(s.split(' '), key = lambda word: word[-1])
