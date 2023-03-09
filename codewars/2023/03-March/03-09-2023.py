f33adcf26e0dfecc1199c5bd17dfd36850499927
Harvey S <hlsamuel00@yahoo.com>
2023-03-09 10:29:32 -0500

    update file codewars/2023/03-March/03-09-2023.py with new kata solution

--- codewars/2023/03-March/03-09-2023.py
+++ codewars/2023/03-March/03-09-2023.py
@@ -19,3 +19,42 @@ def twos_difference(nums: list[int]) -> list[tuple[int, int]]:
 
 #==============================================================================================================
 
+# You are given a binary tree:
+
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+# Your task is to return the list with elements from tree sorted by levels, which means the root element goes first, then root children (from left to right) are second and third, and so on.
+
+# Return empty list if root is None.
+
+# Example 1 - following tree:
+
+#                  2
+#             8        9
+#           1  3     4   5
+# Should return following list:
+
+# [2,8,9,1,3,4,5]
+# Example 2 - following tree:
+
+#                  1
+#             8        4
+#               3        5
+#                          7
+# Should return following list:
+
+# [1,8,4,3,5,7]
+
+def tree_by_levels(tree: Node) -> list[int]:
+    queue, node_list = [tree], []
+    
+    while queue:
+        node = queue.pop(0)
+        if node:
+            queue += [node.left, node.right]
+            node_list.append(node.value)
+            
+    return node_list
