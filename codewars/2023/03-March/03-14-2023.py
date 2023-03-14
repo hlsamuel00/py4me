f807470885b580bd458cf038be4ae8f1e8b33a2a
Harvey S <hlsamuel00@yahoo.com>
2023-03-14 09:43:17 -0400

    update codewars/2023/03-March/03-14-2023.py with new kata challenge

--- codewars/2023/03-March/03-14-2023.py
+++ codewars/2023/03-March/03-14-2023.py
@@ -8,3 +8,22 @@ def sorter(textbooks: list[str]) -> list[str]:
 
 #==============================================================================================================
 
+# DESCRIPTION:
+# Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+
+# Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
+
+# Note that numbers written as strings are strings and must be sorted with the other strings.
+
+def db_sort(arr: list) -> list:
+    nums = [num for num in arr if type(num) is int]
+    strings = [string for string in arr if type(string) is str]
+    return sorted(nums) + sorted(strings)
+
+# OR
+
+def db_sort(arr: list) -> list:
+    return sorted(arr, key = lambda item: (isinstance(item, str), item))
+
+
+#==============================================================================================================
