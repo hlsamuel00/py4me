eb2049f1da081710690347752be135213c87db7c
Harvey S <hlsamuel00@yahoo.com>
2023-03-14 09:43:29 -0400

    update codewars/2023/03-March/03-14-2023.py with new kata challenge

--- codewars/2023/03-March/03-14-2023.py
+++ codewars/2023/03-March/03-14-2023.py
@@ -27,3 +27,15 @@ def db_sort(arr: list) -> list:
 
 
 #==============================================================================================================
+
+# In this kata, you will sort elements in an array by decreasing frequency of elements. If two elements have the same frequency, sort them by increasing value.
+
+# solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+# -- We sort by highest frequency to lowest frequency.
+# -- If two elements have same frequency, we sort by increasing value.
+# More examples in test cases.
+
+# Good luck!
+
+def solve(arr: list[int]) -> list[int]:
+    return sorted(arr, key = lambda num : (-arr.count(num), num))
