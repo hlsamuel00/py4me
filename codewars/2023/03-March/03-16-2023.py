61db56400740e1f13734fe8f37163039288b7380
Harvey S <hlsamuel00@yahoo.com>
2023-03-16 15:07:02 -0400

    update file codewars/2023/03-March/03-16-2023.py with new kata challenge

--- codewars/2023/03-March/03-16-2023.py
+++ codewars/2023/03-March/03-16-2023.py
@@ -98,3 +98,19 @@ def order_word(s: str) -> str:
 
 #==============================================================================================================
 
+# The alphabetized kata
+# Re-order the characters of a string, so that they are concatenated into a new string in "case-insensitively-alphabetical-order-of-appearance" order. Whitespace and punctuation shall simply be removed!
+
+# The input is restricted to contain no numerals and only words containing the english alphabet letters.
+
+# Example:
+
+# alphabetized("The Holy Bible") # "BbeehHilloTy"
+
+def alphabetized(s: str) -> str:
+    return ''.join(sorted((char for char in s if char.isalpha()), key=str.lower))
+
+# OR
+
+def alphabetized(s: str) -> str:
+    return ''.join(sorted(filter(str.isalpha, s), key = str.lower))
