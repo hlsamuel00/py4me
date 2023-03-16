e4ea6ffbd7df35d253ac09e5c4ffae3c9028b963
Harvey S <hlsamuel00@yahoo.com>
2023-03-16 13:19:53 -0400

    update file codewars/2023/03-March/03-16-2023.py with new kata challenge

--- codewars/2023/03-March/03-16-2023.py
+++ codewars/2023/03-March/03-16-2023.py
@@ -11,3 +11,13 @@ def sort_array(value: str) -> str:
     return "".join(sorted(value))
 
 #==============================================================================================================
+
+# DESCRIPTION:
+# Christmas is coming, and Santa has a long list to go through, to find who deserves presents for the big day. Go through a list of children, and return a list containing every child who appeared on Santa's list. Do not add any child more than once. Output should be sorted.
+
+# Comparison should be case sensitive and the returned list should contain only one copy of each name: "Sam" and "sam" are different, but "sAm" and "sAm" are not.
+
+def find_children(santas_list: list[str], children: list[str]):
+    return sorted(child for child in set(children) if child in santas_list)
+
+#==============================================================================================================
