436c1d0a62e9be720919df9c4a5227698cfb48e6
Harvey S <hlsamuel00@yahoo.com>
2023-03-16 14:40:05 -0400

    update file codewars/2023/03-March/03-16-2023.py with new kata challenge

--- codewars/2023/03-March/03-16-2023.py
+++ codewars/2023/03-March/03-16-2023.py
@@ -75,3 +75,26 @@ def barista(coffees: list[int]) -> list[int]:
     return total
 
 #==============================================================================================================
+
+# DESCRIPTION:
+# Description:
+# Given a string, you need to write a method that order every letter in this string in ascending order.
+
+# Also, you should validate that the given string is not empty or null. If so, the method should return:
+
+# "Invalid String!"
+# Notes
+# • the given string can be lowercase and uppercase.
+# • the string could include spaces or other special characters like '# ! or ,'. Sort them based on their ASCII codes
+# Examples
+# "hello world" => " dehllloorw"
+# "bobby"       => "bbboy"
+# ""            => "Invalid String!"
+# "!Hi You!"    => " !!HYiou"
+# Good luck! Hope you enjoy it
+
+def order_word(s: str) -> str:
+    return 'Invalid String!' if not s else ''.join(sorted(s))
+
+#==============================================================================================================
+
