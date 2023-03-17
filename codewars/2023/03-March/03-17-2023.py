cd05a5cfe2b5cdebe5a2b8c921b5af8ad400fafe
Harvey S <hlsamuel00@yahoo.com>
2023-03-17 13:03:58 -0400

    update file codewars/2023/03-March/03-17-2023.py with new kata challenge

--- codewars/2023/03-March/03-17-2023.py
+++ codewars/2023/03-March/03-17-2023.py
@@ -63,3 +63,34 @@ def sort_by_value_and_index(arr: list[int]) -> list[int]:
     return [val for _, val in sorted(enumerate(arr, 1), key = lambda tup: tup[0] * tup[1])]
 
 #=============================================================================================================
+
+# DESCRIPTION:
+# Suzuki needs help lining up his students!
+
+# Today Suzuki will be interviewing his students to ensure they are progressing in their training. He decided to schedule the interviews based on the length of the students name in descending order. The students will line up and wait for their turn.
+
+# You will be given a string of student names. Sort them and return a list of names in descending order.
+
+# Here is an example input:
+
+# string = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'
+# Here is an example return from your function:
+
+#  lst = ['Takehiko',
+#         'Takayuki',
+#         'Takahiro',
+#         'Takeshi',
+#         'Takeshi',
+#         'Takashi',
+#         'Tadashi',
+#         'Takeo',
+#         'Takao']
+# Names of equal length will be returned in reverse alphabetical order (Z->A) such that:
+
+# string = "xxa xxb xxc xxd xa xb xc xd"
+# Returns
+
+# ['xxd', 'xxc', 'xxb', 'xxa', 'xd', 'xc', 'xb', 'xa']
+
+def lineup_students(names: str):
+    return sorted(names.split(), key = lambda name: (len(name), name), reverse = True)
