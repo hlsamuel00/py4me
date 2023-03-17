691a8f5ebed462649cd80771a4d9d29b3a705c58
Harvey S <hlsamuel00@yahoo.com>
2023-03-17 12:51:09 -0400

    update file codewars/2023/03-March/03-17-2023.py with new kata challenge

--- codewars/2023/03-March/03-17-2023.py
+++ codewars/2023/03-March/03-17-2023.py
@@ -39,3 +39,27 @@ def sort_twisted37(arr: list[int]) -> list[int]:
     return sorted(arr, key = twisted )
 
 #==============================================================================================================
+
+# Sort an array by value and index
+# Your task is to sort an array of integer numbers by the product of the value and the index of the positions.
+
+# For sorting the index starts at 1, NOT at 0!
+# The sorting has to be ascending.
+# The array will never be null and will always contain numbers.
+
+# Example:
+
+# Input: 23, 2, 3, 4, 5
+# Product of value and index:
+# 23 => 23 * 1 = 23  -> Output-Pos 4
+#  2 =>  2 * 2 = 4   -> Output-Pos 1
+#  3 =>  3 * 3 = 9   -> Output-Pos 2
+#  4 =>  4 * 4 = 16  -> Output-Pos 3
+#  5 =>  5 * 5 = 25  -> Output-Pos 5
+
+# Output: 2, 3, 4, 23, 5
+
+def sort_by_value_and_index(arr: list[int]) -> list[int]:
+    return [val for _, val in sorted(enumerate(arr, 1), key = lambda tup: tup[0] * tup[1])]
+
+#=============================================================================================================
