cd2e907d0bf168893015eae679c0bde1e82ca9d3
Harvey S <hlsamuel00@yahoo.com>
2023-03-27 17:02:00 -0400

    update file codewars/2023/03-March/03-27-2023.py with new kata challenge

--- codewars/2023/03-March/03-27-2023.py
+++ codewars/2023/03-March/03-27-2023.py
@@ -102,3 +102,17 @@ def eliminate_unset_bits(number: str) -> int:
     return 2 ** number.count('1') - 1
 
 #==============================================================================================================
+
+# Complete the method that returns true if 2 integers share at least two 1 bits, otherwise return false. For simplicity assume that all numbers are non-negative.
+
+# Examples
+#  7  =  0111 in binary
+# 10  =  1010
+# 15  =  1111
+# 7 and 10 share only a single 1-bit (at index 2) --> false
+# 7 and 15 share 3 1-bits (at indexes 1, 2, and 3) --> true
+# 10 and 15 share 2 1-bits (at indexes 0 and 2) --> true
+# Hint: you can do this with just string manipulation, but binary operators will make your life much easier.
+
+def shared_bits(a: int, b: int) -> bool:
+    return bin(a & b).count('1') > 1
