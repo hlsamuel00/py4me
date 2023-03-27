11db50a8918802ca70e557979a4bf84ba38f00e6
Harvey S <hlsamuel00@yahoo.com>
2023-03-27 16:05:57 -0400

    update file codewars/2023/03-March/03-27-2023.py with new kata challenge

--- codewars/2023/03-March/03-27-2023.py
+++ codewars/2023/03-March/03-27-2023.py
@@ -47,3 +47,32 @@ def convert_bits(a:int, b:int) -> int:
     return bin(a ^ b).count('1')
 
 #==============================================================================================================
+
+# DESCRIPTION:
+# Task
+# You are given two numbers a and b where 0 ≤ a ≤ b. Imagine you construct an array of all the integers from a to b inclusive. You need to count the number of 1s in the binary representations of all the numbers in the array.
+
+# Example
+# For a = 2 and b = 7, the output should be 11
+
+# Given a = 2 and b = 7 the array is: [2, 3, 4, 5, 6, 7]. Converting the numbers to binary, we get [10, 11, 100, 101, 110, 111], which contains 1 + 2 + 1 + 2 + 2 + 3 = 11 1s.
+
+# Input/Output
+# [input] integer a
+# Constraints: 0 ≤ a ≤ b.
+
+# [input] integer b
+# Constraints: a ≤ b ≤ 100.
+
+# [output] an integer
+
+def range_bit_count(a: int, b: int) -> int:
+    return sum(bin(num).count('1') for num in range(a, b + 1))
+
+# OR
+
+def range_bit_count(a: int, b: int) -> int:
+    return sum(num.bit_count() for num in range(a, b + 1))
+
+#==============================================================================================================
+
